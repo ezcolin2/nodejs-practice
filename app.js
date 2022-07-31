cccbc2e3a0c06ed34b790b44d1de6109854e4df2
you <growth_s@naver.com>
2022-07-31 22:10:32 +0900

    folder edit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.set("views","./views");
+app.set("views","./src/views");
 app.set("view engine","ejs");
 
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 app.use("/",home);
 module.exports = app;
