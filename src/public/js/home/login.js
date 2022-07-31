d18601ee787c56f5684e72319c94d4de6e1d4757
you <growth_s@naver.com>
2022-07-31 22:58:54 +0900

    로그인 요쳥 api 생성

--- src/public/js/home/login.js
+++ src/public/js/home/login.js
@@ -9,5 +9,13 @@ const login = ()=>{
         id : id.value,
         pwd : pwd.value
     };
+    fetch("/login",{
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(req)
+
+    })
 };
 btn.addEventListener("click",login);
